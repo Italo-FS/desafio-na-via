77e70d4da572e034dc7549d2156c35654d37e4c3
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-05-29 07:34:57 -0300

    salvando edições das páginas de multiplayer

--- js/hosting.js
+++ js/hosting.js
@@ -1,4 +1,9 @@
+const button_p2p = document.querySelector('.mode__button_p2p_copy');
+
 function createoffer() {
+	console.log('clickcreateoffer');
+	// document.getElementById('buttoncreateoffer').disabled = true;
+	// document.getElementById('spanoffer').classList.toggle('invisible');
 	peerConnection = createPeerConnection(lasticecandidate);
 	dataChannel = peerConnection.createDataChannel('chat');
 	dataChannel.onopen = datachannelopen;
@@ -6,31 +11,75 @@ function createoffer() {
 	createOfferPromise = peerConnection.createOffer();
 	createOfferPromise.then(createOfferDone, createOfferFailed);
 }
-
+  
 function createOfferDone(offer) {
+	console.log('createOfferDone');
 	setLocalPromise = peerConnection.setLocalDescription(offer);
-	setLocalPromise.then(setLocalDone, setLocalFailed(offer));
+	setLocalPromise.then(setLocalDone, setLocalFailed);
 }
 
 function createOfferFailed(reason) {
 	console.log('createOfferFailed');
 	console.log(reason);
-	clickcreateoffer();
 }
-
+  
 function setLocalDone() {
 	console.log('setLocalDone');
 }
-
-function setLocalFailed(reason, offer) {
+  
+function setLocalFailed(reason) {
 	console.log('setLocalFailed');
 	console.log(reason);
-	createOfferDone(offer);
 }
 
 function lasticecandidate() {
+	console.log('lasticecandidate');
 	textelement = document.getElementById('textoffer');
 	offer = peerConnection.localDescription;
-	textelement.value = JSON.stringify(offer);
-	document.getElementById('buttonoffersent').disabled = false;
+	textelement.innerHTML = JSON.stringify(offer);
+
+	document.querySelector('.token__loading').classList.add("invisible");
+	document.querySelector('.token__content').classList.remove('invisible');
+	document.querySelector('.mode__button_p2p_copy').classList.remove('invisible');
 }
+
+function clickoffersent() {
+	console.log('clickoffersent');
+	document.getElementById('spananswer').classList.toggle('invisible');
+	document.getElementById('buttonoffersent').disabled = true;
+}
+
+function clickanswerpasted() {
+	console.log('clickanswerpasted');
+	document.getElementById('buttonanswerpasted').disabled = true;
+	textelement = document.getElementById('textanswer');
+	textelement.readOnly = true;
+	answer = JSON.parse(textelement.value);
+	setRemotePromise = peerConnection.setRemoteDescription(answer);
+	setRemotePromise.then(setRemoteDone, setRemoteFailed);
+}
+
+function setRemoteDone() {
+	console.log('setRemoteDone');
+}
+
+function setRemoteFailed(reason) {
+	console.log('setRemoteFailed');
+	console.log(reason);
+}  
+
+function copyToken() {
+	var text = document.getElementById("textoffer").value;
+
+	navigator.clipboard.writeText(text)
+	.then(() => {
+		console.log('Text copied to clipboard');
+	})
+	.catch(err => {
+		console.error('Error in copying text: ', err);
+	});
+}
+
+button_p2p.addEventListener('click', function () {
+	copyToken();
+});
